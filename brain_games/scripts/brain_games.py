8150cfac07bb42b2bf79a95a2a561945bfad0a94
alenavino <alenavino1996@gmail.com>
2024-02-16 11:03:18 +0300

    add welcome_user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
 
+from brain_games.cli import welcome_user
+
+
 def greet():
     print('Welcome to the Brain Games!')
 
 
 def main():
     greet()
+    welcome_user()
 
 
 if __name__ == '__main__':
     main()
+
+
