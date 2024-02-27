4902a4f91c23407c71afe473e5d95a587700e748
alenavino <alenavino1996@gmail.com>
2024-02-27 09:13:09 +0300

    fix progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,15 +5,15 @@ def progression():
     exercise = 'What number is missing in the progression?'
     first_number = randint(1, 100)
     difference = randint(1, 100)
-    list = [first_number]
-    while len(list) < 10:
-        last_number = list[-1]
-        list.append(last_number + difference)
+    lst = [first_number]
+    while len(lst) < 10:
+        last_number = lst[-1]
+        lst.append(last_number + difference)
     secret = randint(1, 10)
-    correct_answer = list[secret]
-    list[secret] = '..'
+    correct_answer = lst[secret]
+    lst[secret] = '..'
     expression = ''
-    for element in list:
+    for element in lst:
         expression += str(element)
         expression += ' '
     return exercise, expression, correct_answer
