2dd6783827194281372f1fed19bedbf090901257
alenavino <alenavino1996@gmail.com>
2024-03-01 14:04:08 +0300

    fix engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,9 +1,9 @@
-from brain_games.cli import welcome_user
 import prompt
 
 
 def engine(game):
-    user_name = welcome_user()
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
     i = 1
     while i < 4:
         exercise, expression, correct_answer = game()
@@ -16,8 +16,8 @@ def engine(game):
         else:
             print(f"'{answer}' is wrong answer ;(. \
 Correct answer was '{correct_answer}'")
-            print(f"Let's try again, {user_name}!")
+            print(f"Let's try again, {name}!")
             break
         if i == 3:
-            print(f'Congratulations, {user_name}!')
+            print(f'Congratulations, {name}!')
         i += 1
