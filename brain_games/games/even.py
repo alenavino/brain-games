6a240d1873b67c06787cd959ab33533595dcca65
alenavino <alenavino1996@gmail.com>
2024-03-01 13:58:56 +0300

    fix even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,12 +1,16 @@
 from random import randint
 
 
+def is_even(num):
+    return num % 2 == 0
+
+
 def even():
     exercise = 'Answer "yes" if the number is even, \
 otherwise answer "no".'
     number = randint(1, 100)
     correct_answer = ''
-    if number % 2 == 0:
+    if is_even(number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
